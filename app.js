79560a05423ffcebd2aa3f1c0ba6a40971bb6ef5
Joan Anton <jantonsg@gmail.com>
2014-07-31 00:07:58 +0200

    Login between a service

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 var app = angular.module('angularLogin', []);
 
-app.controller('angularLoginController', 
-	['$scope','$window','$location', function
-	( $scope , $window , $location ){
+//CONFIG
+app.value('USERNAME', 'miusername');
+app.value('PASSWORD', 'mipassword');
+
+//CONTROLLERS
+app.controller('angularLoginController',
+	['$scope','$window','$location','LoginService', function
+	( $scope , $window , $location , LoginService ){
 
 		$scope.login = function(){
-			var mensaje = 'Hola ' + $scope.user.username + ', bienvenid@ a nuestra plataforma';
-			// Put the object into storage
-			$window.alert(mensaje);
-			$location.path('/board');
+			
+			if (LoginService.checkLogin($scope.user)){
+				var mensaje = 'Hola ' + $scope.user.username + ', bienvenid@ a nuestra plataforma';
+				localStorage.setItem('user', {'username': $scope.username, 'isLogged':true});
+				$window.alert(mensaje);
+				$location.path('/board');
+			}else{
+				$window.alert('Your credentials are wrong');
+				$location.path('/');
+			}
+			
 		}
 
 		$scope.user = {
 			username: 'Maria',
 			password: 'patata'
 		}
+}]);
+
+//SERVICES
+app.factory('LoginService', 
+	['USERNAME','PASSWORD', function
+	( USERNAME , PASSWORD) {
+
+		function checkLogin(user){
+			if (user.username === USERNAME && user.password === PASSWORD){
+				return true;
+			}
+		}
+
+		return {
+			checkLogin: checkLogin
+		};
+
 }]);
